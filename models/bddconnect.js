a0183a29070e977e634fce1812e3ed37fbbcb0e5
Romane Castera <romanecstr@gmail.com>
2021-02-17 17:15:06 +0000

    yams part 10

--- models/bddconnect.js
+++ models/bddconnect.js
@@ -1,13 +1,10 @@
 var mongoose = require('mongoose');
-
-var user = 'romane';
-var password = 'roma04';
-var bddname = 'yams';
+const URI = require('./URI');
 
 var options = { connectTimeoutMS: 5000, useNewUrlParser: true, useUnifiedTopology: true  }
 
 mongoose.connect(
-  `mongodb+srv://${user}:${password}@cluster0.bu6a5.mongodb.net/${bddname}?retryWrites=true&w=majority`, 
+  URI, 
   options, 
   function(error){
     console.log(error)
